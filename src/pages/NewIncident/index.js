6defd10bac3c984219a7f6e231289a972752c6c9
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-25 23:48:30 -0300

    Creating new incidents

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import logoImage from "../../assets/logo.svg";
 import "./style.css";
+import api from "../../services/api";
 
 function NewIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+
+  const history = useHistory();
+  const ngoId = localStorage.getItem("ngoId");
+
+  async function handleNewIncident(event) {
+    event.preventDefault();
+    const data = {
+      title,
+      description,
+      value
+    };
+
+    try {
+      await api.post("incident", data, {
+        headers: {
+          Authorization: ngoId
+        }
+      });
+      history.push("/profile");
+    } catch (error) {
+      alert(`Sorry, we had a problem, try again.`);
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -17,16 +45,22 @@ function NewIncident() {
             Back to home
           </Link>
         </section>
-        <form>
-          <input placeholder="Incident title" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Incident title"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
           <textarea
             placeholder="Description"
-            name=""
-            id=""
-            cols="30"
-            rows="10"
+            value={description}
+            onChange={event => setDescription(event.target.value)}
           ></textarea>
-          <input placeholder="Contribution in U$" />
+          <input
+            placeholder="Contribution in U$"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
 
           <button className="button" type="submit">
             Cadastrar
